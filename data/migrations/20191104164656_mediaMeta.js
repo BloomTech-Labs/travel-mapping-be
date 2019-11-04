635135da8f99520007597e0fba0770271e8b9bc3
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-04 16:48:32 -0600

    Created mediaMeta schema

---
+++ data/migrations/20191104164656_mediaMeta.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+
+  // mediaMeta Table
+  return knex.schema.createTable('mediaMeta', table => {
+
+    // mediaMeta_id
+    table.bigInteger('mediaMeta_id').primary();
+
+    // media_id
+    table.bigInteger('media_id')
+      .notNullable()
+      .references('media_id')
+      .inTable('media')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    // name
+    table.string('name', 255)
+      .notNullable();
+
+    // value
+    table.text('value', 'longtext')
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('mediaMeta');
+};
