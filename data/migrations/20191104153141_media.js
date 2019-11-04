ecde7ab039745a897fb2258529399b5cc736fe2c
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-04 15:43:59 -0600

    Updated media schema

---
+++ data/migrations/20191104153141_media.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable( 'media', table => {
+
+    // media_id
+    table.bigIncrements('media_id').primary();
+
+    // user_id
+    table.bigInteger('user_id')
+      .notNullable()
+      .references('user_id')
+      .inTable('users');
+
+    // title
+    table.string('title', 255)
+      .notNullable();
+
+    // caption
+    table.text('caption');
+
+    // type
+    table.enu('type', ['photo', 'video'])
+      .notNullable()
+      .defaultTo('photo');
+
+    // created_at
+    table.timestamp('created_at')
+      .defaultTo(knex.fn.now());
+
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('media')
+};
