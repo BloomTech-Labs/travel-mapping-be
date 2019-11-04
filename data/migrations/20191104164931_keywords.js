e586fbdb7bdaa0e31e2165a255502e590198e675
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-04 17:02:36 -0600

    Created keywords schema

---
+++ data/migrations/20191104164931_keywords.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  
+  // Keywords Table
+  return knex.schema.createTable('keywords', table => {
+
+    // keyword_id
+    table.bigIncrements('keyword_id').primary();
+
+    // name
+    table.string('name', 255)
+      .notNullable();
+
+      // created_at
+      table.timestamp('created_at')
+        .defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('keywords');
+};
