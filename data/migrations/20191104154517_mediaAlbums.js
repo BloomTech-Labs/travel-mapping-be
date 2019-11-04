fadaf40de7125dcde40dccb83e1244e66ee9b8ce
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-04 16:01:08 -0600

    Updated mediaAlbums schema

--- data/migrations/20191104154517_mediaAlbums.js
+++ data/migrations/20191104154517_mediaAlbums.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+
+  // mediaAlbums Table
+  return knex.schema.createTable('mediaAlbums', table => {
+
+    // media_id
+    table.bigInteger('media_id')
+      .notNullable()
+      .references('id')
+      .inTable('media')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    // album_id
+    table.bigInteger('album_id')
+      .notNullable()
+      .references('id')
+      .inTable('albums')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('mediaAlbums')
 };
