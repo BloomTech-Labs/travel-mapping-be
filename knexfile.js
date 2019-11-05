51ae436db05ffc762a485d27240d3921065680d3
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-04 19:03:32 -0600

    Updated knexfile.js with PostgreSQL connection

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 module.exports = {
+  
     development: {
       client: "sqlite3",
       useNullAsDefault: true, // needed for sqlite
@@ -19,9 +20,37 @@ module.exports = {
         },
       }
     },
+
     staging: {
       client: 'pg',
-      connection: process.env.DATABASE_URL
-    } 
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+        },
+      }
+    },
+
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+        },
+      }
+    }
   }
     
