384a671a8b29f469e649acf601d5faddcaeec17c
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-05 12:36:42 -0600

    Added test table migration

---
+++ data/migrations/20191105123204_test.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+  
+  // Test Table
+  return knex.schema.createTable('test', table => {
+
+    // test_id
+    table.bigIncrements('test_id').primary();
+
+    // name
+    table.string('name', 255)
+      .notNullable();
+
+    // title
+    table.string('title', 255)
+      .notNullable();
+
+    // description
+    table.string('description')
+      .notNullable();
+
+    // created_at
+    table.timestamp('created_at')
+      .defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  return knex.dropTableIfExists('test');
+};
