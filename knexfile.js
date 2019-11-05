421241c840b8ec8a4e0a7dc78bf9fdf37785ed11
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-05 11:52:56 -0600

    Added Procfile and superuser

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
 
     development: {
       client: "sqlite3",
-      useNullAsDefault: true, // needed for sqlite
+      useNullAsDefault: true,
       connection: {
         filename: "./data/piktorlog.db3"
       },
@@ -13,7 +13,6 @@ module.exports = {
       seeds: {
         directory: "./data/seeds"
       },
-      // add the following
       pool: {
         afterCreate: (conn, done) => {
           // runs after a connection is made to the sqlite engine
