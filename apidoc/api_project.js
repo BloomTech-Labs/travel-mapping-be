c7b4a0d3ab07cc3aa998a3044c599942bfa75069
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-07 15:15:58 -0600

    Built documentation

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Piktorlog API Reference",
+  "version": "0.1.0",
+  "description": "This documentation describes the Piktorlog API.",
+  "title": "API | Piktorlog",
+  "url": "https://piktorlog.herokuapp.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-11-07T21:15:04.025Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
