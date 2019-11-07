3d34fb8166c502b4f554f239c82c84a3cadb2f54
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2019-11-06 20:51:21 -0800

    Commit package-lock, add linter

--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,38 @@
-
 module.exports = {
-
-    development: {
-      client: "sqlite3",
-      useNullAsDefault: true, // needed for sqlite
-      connection: {
-        filename: "./data/piktorlog.db3"
-      },
-      migrations: {
-        directory: "./data/migrations"
-      },
-      seeds: {
-        directory: "./data/seeds"
-      },
-      // add the following
-      pool: {
-        afterCreate: (conn, done) => {
-          // runs after a connection is made to the sqlite engine
-          conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-        },
-      }
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+      filename: "./data/piktorlog.db3"
     },
-
-    [process.env.RUN_ENV]: {
-      client: 'pg',
-      connection: process.env.DATABASE_URL,
-      migrations: {
-        directory: "./data/migrations"
-      },
-      seeds: {
-        directory: "./data/seeds"
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run("PRAGMA foreign_keys = ON", done);
-        },
-      }
+    migrations: {
+      directory: "./data/migrations"
     },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    // add the following
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    }
+  },
+
+  [process.env.RUN_ENV]: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   }
-    
+};
