a837d95ba298d8889e17ab17020c7e880a88c688
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-07 22:20:43 -0600

    Added an integration test for /users

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,5 +5,5 @@ const Sentry  = require('@sentry/node');
 
 // POST HTTP/1.1 201 CREATED
 router.post('/users', (req, res, next) => {
-
+  res.send('hello world');
 });
--- test/endpoints/user.js
+++ test/endpoints/user.js
@@ -1,10 +1,23 @@
+const environment = process.env.NODE_ENV || 'development';
+const server = environment === 'testing' 
+  ? require('../../server') 
+  : 'http://localhost:4001';
+const chaiHttp = require('chai-http');
+const chai     = require('chai');
+const expect   = chai.expect;
+
+chai.use(chaiHttp);
 
 describe('User endpoint integration tests', () => {
 
   describe('/users', () => {
 
-    it('should pass', done => {
-      done();
+    it('should respond with hello world', done => {
+      chai.request(server)
+          .post('/users')
+          .then(() => {
+            done();
+          });
     });
 
   });
