edee643fe674a7837a5fe8129ecf2784a1aacffc
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-07 20:49:34 -0600

    Updated file structure

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router  = express.Router();
 const api     = require('../middleware/middleware');
 const Sentry  = require('@sentry/node');
+
+// POST HTTP/1.1 201 CREATED
+router.post('/users', (req, res, next) => {
+
+});
---
+++ test/endpoints/user.js
@@ -0,0 +1,12 @@
+
+describe('User endpoint integration tests', () => {
+
+  describe('/users', () => {
+
+    it('should pass', done => {
+      done();
+    });
+
+  });
+
+});
