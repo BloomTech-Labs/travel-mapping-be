c922b0965252086694bd55e2f0b51021b7a5935a
Tim Dill <brodt7258@gmail.com>
2019-11-13 01:04:31 -0500

    further cleaned up merge

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const api     = require('../middleware/middleware');
+const api     = require('../controllers/controllers');
 
 router.post('/upload', api.upload);
 
--- controllers/routes/upload.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const api     = require('../middleware/middleware');
-
-router.post('/upload', api.upload);
-
-module.exports = router;
