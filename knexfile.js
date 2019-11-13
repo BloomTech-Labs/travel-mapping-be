86b24f692f41236a79370e5d2aedb9ffc8ddd3fd
Brandon G. Elzy <brandon.g.elzy@gmail.com>
2019-11-12 22:38:53 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,27 @@ module.exports = {
         },
       }
     },
-
+    
+    testing: {
+      client: "sqlite3",
+      useNullAsDefault: true,
+      connection: {
+        filename: "./data/piktorlog.db3"
+      },
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          // runs after a connection is made to the sqlite engine
+          conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+        },
+      }
+    },
+    
     review: {
       client: 'pg',
       connection: process.env.DATABASE_URL,
