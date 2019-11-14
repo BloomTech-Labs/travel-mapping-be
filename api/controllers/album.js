e7701599660fb9bff9bb01b8dc9fd4cf10aa2a63
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-13 23:25:39 -0600

    Configured CORS for all environments

---
+++ api/controllers/album.js
@@ -0,0 +1,4 @@
+const validate = require('../../modules/modules').validate;
+const errors   = require('../../modules/modules').errors;
+const user     = require('../../data/models/models').user;
+const jwt      = require('jsonwebtoken');
