7107a5e6ae1e9e9f77403519dbd5988bc5caacdf
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-13 20:46:45 -0600

    Fixed a bug

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -65,8 +65,6 @@ const registerUser = (req, res, next) => {
 
       user.createUser(req.body, (createErr, userIdArr) => {
 
-        // res.json({ userIdArr: userIdArr });
-
         if(createErr) next(createErr);
         else {
   
