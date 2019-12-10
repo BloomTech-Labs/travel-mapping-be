f1a93c9778d87b7f8ae5e314f6049428a6ea53bc
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-12-10 00:57:21 -0500

    revised filter middleware to inclide mediaobjarrt

--- api/middleware/filterAlbums.js
+++ api/middleware/filterAlbums.js
@@ -6,12 +6,12 @@ const filterAlbum = (req, res, next) => {
     const album_id = parseInt(req.params.album_id);
 
 
-    models.album.retrieveAlbumById(album_id, ( albumObj, keywordsArr ) => {
+    models.album.retrieveAlbumById(album_id, ( albumObj, keywordsArr, mediaObjArr, ) => {
 
-   if (albumObj.album_id && keywordsArr.name ){
-keywordsArr.filter(album =>{
-
-    return album.name.includes(req.body)
+   if (albumObj.album_id && keywordsArr.name && mediaObjArr ){
+mediaObjArr.filter(album =>{
+    
+    return album.name.includes(album)
     
 
 })
