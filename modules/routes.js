2034370a41db620478f617e4803ef1b08158c5f0
Tim Dill <brodt7258@gmail.com>
2019-12-30 05:29:38 -0500

    change name of an album route

--- modules/routes.js
+++ modules/routes.js
@@ -18,8 +18,8 @@ module.exports = {
   removeAlbum:      (album_id) => (typeof album_id !== 'undefined' ? `/albums/${ album_id }/remove`      : '/albums/:album_id/remove'),
   createAlbum:      (user_id)  => (typeof user_id  !== 'undefined' ? `/users/${ user_id }/albums/create` : '/users/:user_id/albums/create'),
   editAlbum:        (album_id) => (typeof album_id !== 'undefined' ? `/albums/${ album_id }/edit`        : '/albums/:album_id/edit'),
-  getUsersAlbum:    (user_id, album_id) => (typeof user_id !== 'undefined' && typeof album_id !== 'undefined' ? `/users/${ user_id }/albums/${ album_id }` : '/users/:user_id/albums/:album_id'), // get data for a specific album
-  editAlbumMeta:    (album_id) => (typeof album_id !== 'undefined' ? `/albums/${ album_id }/meta/edit` : '/albums/:album_id/meta/edit'),     // add/remove metadata from an album
+  getAlbum:         (album_id) => (typeof album_id !== 'undefined' ? `/albums/${ album_id }`             : '/albums/:album_id'),              // get data for a specific album
+  editAlbumMeta:    (album_id) => (typeof album_id !== 'undefined' ? `/albums/${ album_id }/meta/edit`   : '/albums/:album_id/meta/edit'),    // add/remove metadata from an album
 
   // Media
   getAlbumsMedia: (album_id) => (typeof album_id !== 'undefined' ? `/albums/${ album_id }/media`      : '/albums/:album_id/media'), // Send media that belongs to an album
